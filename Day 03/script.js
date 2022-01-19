25a2f3ce55e3110597ede8fffe97542cdd2201b8
Vaibhav Sompura <42895093+vsompura3@users.noreply.github.com>
2022-01-20 00:49:27 +0530

    Day03 - Created header and navbar

--- Day 03/script.js
+++ Day 03/script.js
@@ -1,17 +1,1 @@
-const hamburger = document.querySelector('.header-hamburger');
-const closeBtn = document.querySelector('.close-menu');
-const menu = document.querySelector('.mobile-nav');
-
-hamburger.addEventListener('click', function () {
-  menu.classList.add('open');
-})
-
-closeBtn.addEventListener('click', function () {
-  menu.classList.remove('open');
-})
-
-document.addEventListener('keyup', function (e) {
-  if(menu.classList.contains('open') && e.key==='Escape'){
-    menu.classList.remove('open');
-  }
-})
+console.log('Connected!');
