99160475bee959255c9958e2d6b0689ce7bb2344
Vaibhav Sompura <42895093+vsompura3@users.noreply.github.com>
2022-01-20 14:27:00 +0530

    Day03 - Created Header using CSS Grid

--- Day 03/script.js
+++ Day 03/script.js
@@ -1,1 +1,15 @@
 console.log('Connected!');
+
+const navToggle = document.querySelector('.navbar-menu-toggle');
+const nav = document.querySelector('.navbar');
+
+navToggle.addEventListener('click', function () {
+  const visibility = nav.getAttribute('data-visible');
+  if (visibility === 'false') {
+    nav.setAttribute('data-visible', true);
+    navToggle.setAttribute('aria-expanded', true);
+  } else {
+    nav.setAttribute('data-visible', false);
+    navToggle.setAttribute('aria-expanded', false);
+  }
+});
